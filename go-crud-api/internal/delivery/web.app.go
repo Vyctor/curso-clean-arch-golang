2e3a8e5ab0469d9e110579b3bcd51bef20a7ccd2
Vyctor <dev.vyctor@gmail.com>
2025-03-13 00:18:51 -0300

    configurando IoC

--- go-crud-api/internal/delivery/web.app.go
+++ go-crud-api/internal/delivery/web.app.go
@@ -1,1 +1,38 @@
 package delivery
+
+import (
+	"go-crud-api/internal/delivery/dependencies"
+	"go-crud-api/internal/interfaces/handlers"
+	"log"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func Start() {
+	container := dependencies.Setup()
+	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	err := container.Invoke(func(taskHandler *handlers.TaskHandler) {
+		router.POST("/tasks", taskHandler.CreateTask)
+		router.GET("/tasks", taskHandler.GetAllTasks)
+		router.PUT("/tasks/:id", taskHandler.UpdateTask)
+		router.DELETE("/tasks/:id", taskHandler.DeleteTask)
+		log.Println("Server is running at :8080")
+		router.Run(":8080")
+	})
+
+	if err != nil {
+		log.Fatalf("Erro ao resolver dependências: %v", err)
+	}
+}
